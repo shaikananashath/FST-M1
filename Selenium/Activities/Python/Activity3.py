d61e04d2dd266bcaefbffbccecee3a7d82a35dd1
shaikananashath <ananashathshaik2@gmail.com>
2025-12-06 17:01:48 +0530

    Add files via upload

---
+++ Selenium/Activities/Python/Activity3.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+with webdriver.Firefox() as driver:
+    
+    driver.get("https://training-support.net/webelements/login-form")
+    print("Page title is: ", driver.title)
+
+    username = driver.find_element(By.XPATH, "//input[@id='username']")
+    password = driver.find_element(By.XPATH, "//input[@id='password']")
+
+    username.send_keys("admin")
+    password.send_keys("password")
+
+    login = driver.find_element(By.XPATH, "//button[text()='Submit']")
+    login.click()
+
+    message = driver.find_element(By.XPATH, "//h1[contains(@class, 'text-center')]")
+    print("Login message: ", message.text)
