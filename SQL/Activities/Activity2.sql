1c8c0447a1fd5738ba72702a5abe299eb176a048
shaikananashath <ananashathshaik2@gmail.com>
2025-12-30 19:57:24 +0530

    Delete SQL/Activities/Activity2.sql

--- SQL/Activities/Activity2.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO salesman (salesman_id, salesman_name, salesman_city, commission) VALUES
-(5001, 'James Hoog', 'New York', 15),
-(5002, 'Nail Knite', 'Paris', 13),
-(5005, 'Pit Alex', 'London', 11),
-(5006, 'McLyon', 'Paris', 14),
-(5007, 'Paul Adam', 'Rome', 13),
-(5003, 'Lauson Hen', 'San Jose', 12);
